a13d3d385f0ad930dad0d66df628d2b55223b07a
Jack <ngjanhwan@gmail.com>
2024-01-09 16:05:57 +0800

    qol changes

--- database/data_query.py
+++ database/data_query.py
@@ -1,22 +1,30 @@
 import sqlite3
 from datetime import datetime
 
+import sqlite3
+from datetime import datetime
+
+import sqlite3
+from datetime import datetime
+
 def get_next_class():
-    current_day_of_week = datetime.now().strftime('%A')  # Get the current day of the week
-    current_time = datetime.now().strftime('%I:%M %p')  # Get the current time in '12-hour AM/PM' format
+    current_day_of_week = datetime.now().strftime('%A')
+    current_time = datetime.now().strftime('%I:%M %p')
 
     with sqlite3.connect('timetable.db') as connection:
         cursor = connection.cursor()
         cursor.execute('''
             SELECT class_name, day_of_week, class_time, class_location
             FROM timetable
-            WHERE day_of_week = ? AND class_time > ?
-            ORDER BY class_time
+            WHERE (day_of_week = ? AND class_time > ?) OR (day_of_week > ?)
+            ORDER BY class_time, day_of_week
             LIMIT 1
-        ''', (current_day_of_week, current_time))
+        ''', (current_day_of_week, current_time, current_day_of_week))
 
         next_class = cursor.fetchone()
-        return next_class
+
+    return next_class
+
 
 def get_all_classes():
     with sqlite3.connect('timetable.db') as connection:
